580194d160d5b434c43b39929eb99551594a9aac
asus\user <ericcheung931@gmail.com>
2017-04-22 15:26:42 +0800

    signinbutton

--- app/src/main/java/com/example/admin/hackust/MainActivity.java
+++ app/src/main/java/com/example/admin/hackust/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.admin.hackust;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import com.google.android.gms.common.SignInButton;
+
 
 public class MainActivity extends AppCompatActivity {
-
+private Button joinButton;
+    private SignInButton signInButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        joinButton = (Button) findViewById(R.id.joinButtonlayout);
+// Set the dimensions of the sign-in button.
+        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
     }
+    @Override
+    protected void onStart(){
+
+    }
+    protected void JoinChat(Button joinButton){
+     if ()
+    }
+
+
+
+
+
+
+
+
 }
 
